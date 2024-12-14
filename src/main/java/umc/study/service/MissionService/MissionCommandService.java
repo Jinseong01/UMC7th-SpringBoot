b5907cb88d017b0fcee4b4cea0b97aded6ea0873
jinsung <jinsung528@gmail.com>
2024-12-15 02:37:37 +0900

    feat: 진행 중인 미션 진행 완료로 바꾸기

--- src/main/java/umc/study/service/MissionService/MissionCommandService.java
+++ src/main/java/umc/study/service/MissionService/MissionCommandService.java
@@ -1,8 +1,11 @@
 package umc.study.service.MissionService;
 
 import umc.study.domain.MissionEntity;
+import umc.study.domain.mapping.UserMissionEntity;
 import umc.study.web.dto.MissionRequestDTO;
 
 public interface MissionCommandService {
     public MissionEntity addMission(MissionRequestDTO.MissionAddDTO request, Long storeId);
+
+    UserMissionEntity completeUserMission(Long userMissionId, Long userId);
 }
--- src/main/java/umc/study/service/MissionService/MissionCommandServiceImpl.java
+++ src/main/java/umc/study/service/MissionService/MissionCommandServiceImpl.java
@@ -3,12 +3,22 @@ package umc.study.service.MissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.apiPayload.exception.handler.UserHandler;
+import umc.study.apiPayload.exception.handler.UserMissionHandler;
 import umc.study.converter.MissionConverter;
 import umc.study.domain.MissionEntity;
 import umc.study.domain.StoreEntity;
+import umc.study.domain.UserEntity;
+import umc.study.domain.enums.UserMissionStatus;
+import umc.study.domain.mapping.UserMissionEntity;
 import umc.study.repository.MissionRepository.MissionRepository;
 import umc.study.repository.StoreRepository.StoreRepository;
+import umc.study.repository.UserMissionRepository.UserMissionRepository;
+import umc.study.repository.UserRepository.UserRepository;
 import umc.study.web.dto.MissionRequestDTO;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +26,8 @@ public class MissionCommandServiceImpl implements MissionCommandService {
 
     private final StoreRepository storeRepository;
     private final MissionRepository missionRepository;
+    private final UserMissionRepository userMissionRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
@@ -29,5 +41,22 @@ public class MissionCommandServiceImpl implements MissionCommandService {
         mission.setStore(store);
 
         return missionRepository.save(mission);
+    }
+
+    @Override
+    public UserMissionEntity completeUserMission(Long userMissionId, Long userId) {
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        UserMissionEntity userMission = userMissionRepository.findByIdAndUserEntity(userMissionId, user)
+                .orElseThrow(() -> new UserMissionHandler(ErrorStatus.UM_NOT_FOUND));
+
+        if (userMission.getStatus().equals(UserMissionStatus.COMPLETE)) {
+            throw new UserMissionHandler(ErrorStatus.UM_COMPLETE);
+        }
+
+        userMission.setStatus(UserMissionStatus.COMPLETE);
+
+        return userMissionRepository.save(userMission);
     }
 }
